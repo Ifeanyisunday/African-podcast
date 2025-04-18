da322ecf34c8b8c8b85fc07e1a20c8fad54e4ed2
ifeanyisunday <sundayjnr10@gmail.com>
2025-04-18 02:23:31 +0100

    first commit

--- app/apis/api.tsx
+++ app/apis/api.tsx
@@ -46,38 +46,38 @@ export const api = createApi({
   endpoints: (builder) => ({
     trendingPodcasts: builder.query<Episode, GetPostsQueryArgs>({
       query: ({ page = 1, per_page = 15 }) =>
-        `top-podcasts?page=${page}&per_page=${per_page}`,
+        `${apiUrl}top-podcasts?page=${page}&per_page=${per_page}`,
       transformResponse: (response: { data: { data: Episode } }) => response.data.data,
     }),
     trendingEpisodes: builder.query<Episode, GetPostsQueryArgs>({
       query: ({ page, per_page }) =>
-        `popular-and-trending-podcasts?page=${page}&per_page=${per_page}`,
+        `${apiUrl}popular-and-trending-podcasts?page=${page}&per_page=${per_page}`,
       transformResponse: (response: { data: { data: Episode } }) => response.data.data,
     }),
     topCategories: builder.query<Episode, TopCategoryQueryArgs>({
-      query: ({ category }) => `top-categories?category=${category}`,
+      query: ({ category }) => `${apiUrl}top-categories?category=${category}`,
       transformResponse: (response: { data: { data: Episode } }) => response.data.data,
     }),
     getALLPodcasts: builder.query<Episode, GetPostsQueryArgs>({
       query: ({ page, per_page }) =>
-        `popular-and-trending-podcasts?page=${page}&per_page=${per_page}`,
+        `${apiUrl}popular-and-trending-podcasts?page=${page}&per_page=${per_page}`,
       transformResponse: (response: { data: { data: Episode } }) => response.data.data,
     }),
     getPodcast: builder.query<Episode, { podcastId: number }>({
-      query: ({ podcastId }) => `podcasts/${podcastId}`,
+      query: ({ podcastId }) => `${apiUrl}podcasts/${podcastId}`,
       transformResponse: (response: { data: Episode }) => response.data,
     }),
     getPodcastEpisodes: builder.query<PaginatedEpisodes, { podcastId: number; page: number; per_page: number }>({
-      query: ({ podcastId,  page, per_page }) => `podcasts/${podcastId}/episodes?page=${page}&per_page=${per_page}`,
+      query: ({ podcastId,  page, per_page }) => `${apiUrl}podcasts/${podcastId}/episodes?page=${page}&per_page=${per_page}`,
       transformResponse: (response: { data: PaginatedEpisodes }) => response.data,
     }),
     getEpisode: builder.query<Episode, { episodeId: number }>({
-      query: ({ episodeId }) => `episodes/${episodeId}`,
+      query: ({ episodeId }) => `${apiUrl}episodes/${episodeId}`,
       transformResponse: (response: { data: Episode }) => response.data,
     }),
     searchPodcasts: builder.query<PaginatedEpisodes, { query: string; page?: number; per_page?: number }>({
       query: ({ query, page = 1, per_page = 15 }) =>
-        `podcast/search?q=${encodeURIComponent(query)}&page=${page}&per_page=${per_page}`,
+        `${apiUrl}podcast/search?q=${encodeURIComponent(query)}&page=${page}&per_page=${per_page}`,
     }),
   }),
 });
